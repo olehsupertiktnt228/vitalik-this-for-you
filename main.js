4511a3080b91dbdf0eaf28b794f87771fbf8ca3e
olehsupertiktnt228 <156704528+olehsupertiktnt228@users.noreply.github.com>
2024-04-08 19:35:50 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,68 @@
+// for(let i=0; i<10; i++){
+//     console.log(i);
+// }
+
+// let i = 0;
+// do{
+//     console.log(i);
+//     i++
+
+// }while(i<10)
+
+// let i = 20;
+// do{
+//     console.log(i);
+//     i++
+
+// }while(i<50)
+
+// let i = 100;
+// while ( i > 1) {
+//     console.log(i);
+//     i = i - 2
+// }
+
+// let arr = []
+// while(arr.length< 100 ){
+//     arr.push(Math.floor(Math.random() * 100));
+// }
+// console.log(arr)
+// let sum = 0;
+// for (let i = 0; i < arr.length; i++){
+//     sum+= arr[i];
+// }
+// console.log((`Сума: ${sum}`))
+
+// let arr = []
+// while(arr.length< 100 ){
+//     arr.push(Math.floor(Math.random() * 100));
+// }
+// console.log(arr)
+// let sum = 0;
+// for (let i = 0; i < arr.length; i = i + 2){
+//     sum+= arr[i];
+// }
+// console.log((`Сума: ${sum}`))
+
+// let arr = []
+// while(arr.length< 100 ){
+//     arr.push(Math.floor(Math.random() * 100));
+// }
+// console.log(arr)
+// let sum = 0;
+// for (let i = 0; i < arr.length; i++){
+//     sum+= arr[i];
+// }
+// console.log((`Сума: ${sum}`))
+
+// let arr = []
+// while(arr.length< 100 ){
+//     arr.push(Math.floor(Math.random() * 100));
+// }
+// console.log(arr)
+// let sum = 0;
+// for (let i = 0; i < arr.length; i = i * 2){
+//     sum+= arr[i];
+// }
+// console.log((`Сума: ${sum}`))
+
